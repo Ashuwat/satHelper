1f9b0d9c1126e972566a1c64e7cdef94eb12d099
Ashuwat <ashutosh.lodd@gmail.com>
2025-02-02 02:16:20 -0500

    moved everything to server

---
+++ server/README.md
@@ -0,0 +1,3 @@
+Initially, I used TF-IDF combined with Nearest Neighbor so that some context is stored by containing the 'repeatability' or frequence of the word and finding the similarities between the sentences. But the sentiment and space of the vector wasn't that big.
+
+Then I moved with a sentence transformer that held was much more powerful, and held 512 nuerons instead of normally 256.
---
+++ server/sentence_transformer.py
@@ -0,0 +1,26 @@
+from helperFunc.pre_processing import parquet_to_tabular
+from sentence_transformers import SentenceTransformer
+import faiss
+import numpy as np
+
+def sentence_transformer():
+    stored_queries = parquet_to_tabular('./dataset/tabular.parquet')
+
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+
+    query_vectors = model.encode(stored_queries, normalize_embeddings=True) 
+
+    query_vectors = np.array(query_vectors).astype('float32') #this should be default
+
+    index = faiss.IndexFlatIP(query_vectors.shape[1])  # cosing similarity
+    index.add(query_vectors)
+
+    # Encode the new query and normalize
+    new_query = """
+    A pizzeria charges $17 for Pizza A and $13 for Pizza B. Ingredient costs are $450 per week for Pizza A and $310 per week for Pizza B. Last week, the pizzeria sold an equal number of both pizza types, and the weekly profit from the sale of each pizza type was the same. If x represents the number of Pizza B sold, what is the value of x?"""
+    new_query_vector = model.encode([new_query], normalize_embeddings=True).astype('float32') #this should be on default
+    distances, indices = index.search(new_query_vector, k=5)
+    best_match_idx = indices[0][0]
+    best_match_score = distances[0][0]
+
+    return f"Best match: {stored_queries[best_match_idx]} (Similarity: {best_match_score:.4f})"
